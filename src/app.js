705ae14d268ae2f8b9f5f1438bb7947e96b81690
Darren <archerydwd@gmail.com>
2014-07-17 07:51:02 -0700

    clean code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
-var cs = 1;
-var roundTotal = 0;
-var runningTotal = 0;
-var count = 1;
-var roundCount = 1;
+var cs, roundTotal, runningTotal, count, roundCount, set, set1, set2, end;
 var scores = [121];
+var rounds = [4];
+reset();
 
-for (var i = 1; i < 121; i++) {
-  scores[i] = 10;
+function reset() {
+  end = false;
+  cs = 1;
+  roundTotal = 0;
+  runningTotal = 0;
+  count = 1;
+  roundCount = 1;
+  set = false;
+  set1 = false;
+  set2 = false;
+  for (var i = 1; i < 121; i++) {
+    scores[i] = 10;
+  }
+  setBody();
+}
+
+function setDelay() {
+  var delay=3000;
+  setTimeout(function() {
+    simply.subtitle(scores[cs] + ' - _ - _');
+  },delay); 
+}
+
+function setBody() {
+  simply.body(' Last Round: ' + roundTotal + '\r\n Total: ' + runningTotal + '\r\n Round end : ' + roundCount);
 }
 
 simply.setText({
@@ -16,7 +37,10 @@ simply.setText({
 }, true);
 
 simply.on('singleClick', function(e) {
-	if (scores[cs] < 10 && e.button == 'up') {
+  if (end) {
+    reset();
+  }
+	else if (scores[cs] < 10 && e.button == 'up') {
     scores[cs] += 1;
     if (count === 1) {
       simply.subtitle(scores[cs] + ' - _ - _');
@@ -59,36 +83,67 @@ simply.on('singleClick', function(e) {
         simply.subtitle(scores[cs-2] + ' - ' + scores[cs-1] + ' - ' + scores[cs]);
       }
       count++;
-      simply.body(' Last Round: ' + roundTotal + '\r\n Total: ' + runningTotal + '\r\n Round end : ' + roundCount);
     }
+    setBody();
 	}
   if (cs === 121) {
-    simply.body(' Congratulations \r\n Your Total Score is: \r\n' + runningTotal);
+    rounds[3] = runningTotal - (rounds[0] + rounds[1] + rounds[2]);
+    simply.title('Congrats!!!');
+    simply.body('Total Score: ' + runningTotal  + '\r\n 1st 30: ' + rounds[0] + '\r\n 2nd 30: ' + rounds[1] + '\r\n 3rd 30: ' + rounds[2]  + '\r\n 4th 30: ' + rounds[3], true);
+    end = true;
+  }
+  if (roundCount === 11 && !set) {
+    rounds[0] = runningTotal;
+    simply.subtitle("first 30: " + rounds[0]);
+    set = true;
+    setDelay();
+  }
+  else if (roundCount === 21 && !set1) {
+    rounds[1] = runningTotal - rounds[0];
+    simply.subtitle("second 30: " + rounds[1]);
+    set1 = true;
+    setDelay();
+  }
+  else if (roundCount === 31 && !set2) {
+    rounds[2] = runningTotal - (rounds[0] + rounds[1]);
+    simply.subtitle("third 30: " + rounds[2]);
+    set2 = true;
+    setDelay();
   }
 });
 
-
-
-
 simply.on('longClick', function(e) {
-  cs--;
-	if (count === 0 && roundCount != 1) {
-		runningTotal = runningTotal - scores[cs];
-		simply.subtitle(scores[cs-2] + ' - ' + scores[cs-1] + ' - ' + scores[cs]);
-		simply.body(' Last Round: ' + roundTotal + '\r\n Total: ' + runningTotal + '\r\n Round end : ' + (--roundCount));
-		count = 2;
-	}
-	else if (count === 1) {
-		runningTotal = runningTotal - scores[cs];
-		simply.subtitle(scores[cs] + ' - _ - _');
-    simply.body(' Last Round: ' + roundTotal + '\r\n Total: ' + runningTotal + '\r\n Round end : ' + roundCount);
-		count--;
-	}
-	else if (count === 2) {
-		runningTotal = runningTotal - scores[cs];
-		simply.subtitle(scores[cs-1] + ' - ' + scores[cs] + ' - _');
-    simply.body(' Last Round: ' + roundTotal + '\r\n Total: ' + runningTotal + '\r\n Round end : ' + roundCount);
-		count--;
-	}
-	simply.vibe();
+  if (end) {
+    reset();
+  }
+  else if (e.button == 'select') {
+    if (count === 1 && roundCount != 1) {
+      scores[cs] = 10;
+      cs--;
+      runningTotal = runningTotal - scores[cs];
+      simply.subtitle(scores[cs-2] + ' - ' + scores[cs-1] + ' - ' + scores[cs]);
+      roundCount--;
+      setBody();
+      count = 3;
+      simply.vibe();
+    }
+    else if (count === 2) {
+      scores[cs] = 10;
+      cs--;
+      runningTotal = runningTotal - scores[cs];
+      simply.subtitle(scores[cs] + ' - _ - _');
+      setBody();
+      count--;
+      simply.vibe();
+    }
+    else if (count === 3) {
+      scores[cs] = 10;
+      cs--;
+      runningTotal = runningTotal - scores[cs];
+      simply.subtitle(scores[cs-1] + ' - ' + scores[cs] + ' - _');
+      setBody();
+      count--;
+      simply.vibe();
+    }
+  }
 });
