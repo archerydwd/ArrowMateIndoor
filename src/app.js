26446c839c1e81d4c058bce42232adcfd2df2882
Darren Daly <archerydwd@gmail.com>
2015-07-10 23:42:01 +0100

    added basic storage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+/*
+author: Darren Daly
+*/
 var cs, roundTotal, runningTotal, count, roundCount, set, set1, set2, end;
 var scores = [121];
 var rounds = [4];
+var key = Date.now();
 reset();
 
 function reset() {
@@ -91,6 +95,7 @@ simply.on('singleClick', function(e) {
 	}
   if (cs === 121) {
     rounds[3] = runningTotal - (rounds[0] + rounds[1] + rounds[2]);
+		localStorage[key] = JSON.stringify(rounds);
     simply.title('Congrats!!!');
     simply.body('Total Score: ' + runningTotal  + '\r\n 1st 30: ' + rounds[0] + '\r\n 2nd 30: ' + rounds[1] + '\r\n 3rd 30: ' + rounds[2]  + '\r\n 4th 30: ' + rounds[3], true);
     end = true;
@@ -99,18 +104,21 @@ simply.on('singleClick', function(e) {
     rounds[0] = runningTotal;
     simply.subtitle("first 30: " + rounds[0]);
     set = true;
+		localStorage[key] = JSON.stringify(rounds);
     setDelay();
   }
   else if (roundCount === 21 && !set1) {
     rounds[1] = runningTotal - rounds[0];
     simply.subtitle("second 30: " + rounds[1]);
     set1 = true;
+		localStorage[key] = JSON.stringify(rounds);
     setDelay();
   }
   else if (roundCount === 31 && !set2) {
     rounds[2] = runningTotal - (rounds[0] + rounds[1]);
     simply.subtitle("third 30: " + rounds[2]);
     set2 = true;
+		localStorage[key] = JSON.stringify(rounds);
     setDelay();
   }
 });
@@ -148,5 +156,24 @@ simply.on('longClick', function(e) {
       count--;
       simply.vibe();
     }
+	}
+	else if (e.button == 'up') {
+		var rnds = JSON.parse(localStorage[key]);
+		var endscore = 0;
+		if (typeof rnds[3] != "undefined"){
+			endscore = rnds[0]+rnds[1]+rnds[2]+rnds[3];
+		}
+		else if (typeof rnds[2] != "undefined"){
+			endscore = rnds[0]+rnds[1]+rnds[2];
+		}
+		else if (typeof rnds[1] != "undefined"){
+			endscore = rnds[0]+rnds[1];
+		}
+		else if (typeof rnds[0] != "undefined"){
+			endscore = rnds[0];
+		}
+		console.log(rnds);
+		simply.title('scores:');
+    simply.body('Total Score: ' + endscore  + '\r\n 1st 30: ' + rnds[0] + '\r\n 2nd 30: ' + rnds[1] + '\r\n 3rd 30: ' + rnds[2]  + '\r\n 4th 30: ' + rnds[3], true);
   }
 });
